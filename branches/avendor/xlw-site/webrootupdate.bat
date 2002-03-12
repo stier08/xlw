c5998005903957d1ccf8f280cb9dd594f8b30d9e
nobody <nobody@948fe115-5b3c-0410-8233-88faca9dced8>
2002-03-12 12:29:40 +0000

    This commit was manufactured by cvs2svn to create branch 'avendor'.

---
+++ branches/avendor/xlw-site/webrootupdate.bat
@@ -0,0 +1,12 @@
+
+if not %1.==. goto NOTNANDO
+set USERNAME=nando
+goto DONE
+
+:NOTNANDO
+set USERNAME=%1
+:DONE
+
+scp -C -r *.html %USERNAME%@shell.sourceforge.net:/home/groups/x/xl/xlw/htdocs
+
+pause
