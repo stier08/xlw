48f4f7a2fd224fdc50b8fdf238b4201892cb505b
nobody <nobody@948fe115-5b3c-0410-8233-88faca9dced8>
2002-04-11 11:39:42 +0000

    This commit was manufactured by cvs2svn to create tag 'R010100-branch-merge2'.

--- xlw/xlw.nsi
+++ xlw/xlw.nsi
@@ -10,7 +10,7 @@
 # $Id$
 
 
-!define VER_NUMBER "1.1.1a0-cvs"
+!define VER_NUMBER "1.1.0"
 
 # HEADER CONFIGURATION COMMANDS
 !ifdef LIGHT
--- xlw/xlw/EXCEL32_API.h
+++ xlw/xlw/EXCEL32_API.h
@@ -30,13 +30,13 @@
 */
 
 //! version hexadecimal number
-#define XLW_HEX_VERSION 0x010101a0
+#define XLW_HEX_VERSION 0x010100f0
 
 //! version string
 #ifdef XLW_DEBUG
-    #define XLW_VERSION "1.1.1a0-cvs-debug"
+    #define XLW_VERSION "1.1.0-debug"
 #else
-    #define XLW_VERSION "1.1.1a0-cvs"
+    #define XLW_VERSION "1.1.0"
 #endif
 
 //! global trace level (may be superseded locally by a greater value)
