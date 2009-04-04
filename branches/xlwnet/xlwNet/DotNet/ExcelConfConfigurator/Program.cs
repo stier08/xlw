4a3bb1b08de7731a31d781241cefb5d9667da42f
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2009-04-04 17:16:15 +0000

    Modified  The ExcelConConfigurator so that ot works out the refernced Assemblies itself and the user doesn't have to maintain the AssembleyList.DEBUG and AssembleyList.Release files, which may have got annoying. Also less for me to document :-)

--- branches/xlwnet/xlwNet/DotNet/ExcelConfConfigurator/Program.cs
+++ branches/xlwnet/xlwNet/DotNet/ExcelConfConfigurator/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Collections;
+using System.Reflection;
+
 
 namespace ExcelConfConfigurator
 {
@@ -14,35 +16,59 @@ namespace ExcelConfConfigurator
 
             try
             {
-                if (args.Length != 3)
+                if (args.Length < 3 || args.Length > 4)
                 {
-                    Console.WriteLine("Usage: ExcelConfConfigurator <excel.exe.conf template> <assembley list file> ");
-                    return 0;
+                    Console.WriteLine("Usage: ExcelConfConfigurator <excel.exe.conf template> <assembley> <outputDir> [<assembley list file>]\n   [<assembley list file>] is optional\n");
+                    return -1;
                 }
-                string ListFileName = args[1];
-                string fname;
                 List<string> AssembleyList = new List<string>();
-           
-                using (StreamReader ListFile = new StreamReader(ListFileName))
+                if (args.Length == 4)
                 {
-                    while ((fname = ListFile.ReadLine()) != null)
+                    string ListFileName = args[3];
+                    string fname;
+
+                    using (StreamReader ListFile = new StreamReader(ListFileName))
                     {
-                        fname = fname.Trim();
-                        if (fname != "")
+                        while ((fname = ListFile.ReadLine()) != null)
                         {
-                            AssembleyList.Add(fname);
+                            fname = fname.Trim();
+                            if (fname != "")
+                            {
+                                AssembleyList.Add(fname);
+                            }
                         }
+
+                    }
+                }
+                string dllName = args[1];
+                Console.WriteLine("Attempting to Load Assembley " + dllName);
+                AssembleyList.Add(dllName);
+                Assembly theLoadedAssembley = Assembly.LoadFrom(dllName);
+                Console.WriteLine("Attempting to get all referenced aseemblies of " + dllName + " that are in the same directory\n");
+                AssemblyName[] ReferencedAssemblies =
+                    theLoadedAssembley.GetReferencedAssemblies();
+                string AssemblyPath = Path.GetDirectoryName(theLoadedAssembley.Location);
+
+                foreach (AssemblyName s in ReferencedAssemblies)
+                {
+                    string tryDll = s.Name + ".dll";
+                    string tryFullDLLPath = Path.Combine(AssemblyPath, tryDll);
+                    if (File.Exists(tryFullDLLPath))
+                    {
+                        Console.WriteLine("Referenced Assembly " + s.Name + " found\n");
+                        AssembleyList.Add(tryDll);
                     }
 
                 }
-                
+               
+
                 Updater.Configurator.Config(args[0], args[2], "Excel.exe.config", AssembleyList.ToArray());
 
                 return 0;
             }
             catch (Exception e)
             {
-                Console.WriteLine("** " + e.Message);
+                Console.WriteLine("********************\n" + e.Message + "\n********************\n");
                 return -1;
             }
         }
