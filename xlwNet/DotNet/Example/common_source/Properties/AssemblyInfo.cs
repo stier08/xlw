5ead8846b1fac59ff381beead8f2ca2705fd5534
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2009-04-05 21:06:21 +0000

    Undergoing the polishing around the edges. Have added the Copyright and warranty disclaimer to all source files.

--- xlwNet/DotNet/Example/common_source/Properties/AssemblyInfo.cs
+++ xlwNet/DotNet/Example/common_source/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System.Reflection;
+﻿/*
+ Copyright (C) 2008 2009  Narinder S Claire
+
+ This file is part of XLWDOTNET, a free-software/open-source C# wrapper of the
+ Excel C API - http://xlw.sourceforge.net/
+ 
+ XLWDOTNET is part of XLW, a free-software/open-source C++ wrapper of the
+ Excel C API - http://xlw.sourceforge.net/
+ 
+ XLW is free software: you can redistribute it and/or modify it under the
+ terms of the XLW license.  You should have received a copy of the
+ license along with this program; if not, please email xlw-users@lists.sf.net
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +24,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Example")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(" This file is part of XLWDOTNET, a free-software/open-source C# wrapper of the Excel C API - http://xlw.sourceforge.net/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Example")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyProduct("xlwDotNet Example")]
+[assembly: AssemblyCopyright("Copyright ©  2008 2009 Narinder S Claire")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
