540e45ea510ebc8876096d114ee51e9b52c2627b
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2009-04-19 21:15:37 +0000

    Was getting fed up with having to go to each solution in each vs environment and building and waiting ...boring. The script BuildAll is for convenience. In 2005 Express need the extra change to the project.

---
+++ branches/xlwnet/BuildAll.bat
@@ -0,0 +1,9 @@
+
+
+
+
+"C:\Program Files\Microsoft Visual Studio 8\VC\vcpackages\vcbuild.exe" /rebuild    xlw\build\vc8\xlw.sln  >> xlw8.log 2>&1 
+"C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages\vcbuild.exe" /rebuild  xlw\build\vc9\xlw.sln  >> xlw9.log 2>&1 
+
+"C:\Program Files\Microsoft Visual Studio 8\VC\vcpackages\vcbuild.exe"  /rebuild   xlwNet\DotNet\Build\VS8\xlw.Net.sln >> xlwDotNet8.log  2>&1 
+"C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages\vcbuild.exe" /rebuild  xlwNet\DotNet\Build\VS9\xlw.Net.sln >> xlwDotNet9.log  2>&1 
