ab46c88528bf18ec186506dfe6e704ce620b5d0d
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2009-04-19 21:55:04 +0000

    include vc7 as well

--- branches/xlwnet/BuildAll.bat
+++ branches/xlwnet/BuildAll.bat
@@ -1,7 +1,8 @@
 
 
 
-
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"   xlw\build\vc7\xlw.sln  /rebuild Debug    /out xlw7Debug.log
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"   xlw\build\vc7\xlw.sln  /rebuild Release  /out xlw7Release.log
 "C:\Program Files\Microsoft Visual Studio 8\VC\vcpackages\vcbuild.exe" /rebuild    xlw\build\vc8\xlw.sln  >> xlw8.log 2>&1 
 "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcpackages\vcbuild.exe" /rebuild  xlw\build\vc9\xlw.sln  >> xlw9.log 2>&1 
 
