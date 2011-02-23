ddeebf2d459348efb25ff18f5a3ab7b8b2f932e8
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2011-02-23 06:05:25 +0000

    xlw ( not xlwDotNet) seems to work fine for vc2010

---
+++ Dev5/xlw/BatchBuild/BuildOnlyVS10.bat
@@ -0,0 +1,4 @@
+
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /p:Configuration=Debug /t:rebuild  ..\xlw\build\vc10\xlw.sln  >> xlw10_Debug.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /p:Configuration=Release /t:rebuild  ..\xlw\build\vc10\xlw.sln  >> xlw10_Release.log 2>&1 
+REM "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcpackages\vcbuild.exe" /rebuild  ..\xlwDotNet\Build\VS9\xlw.Net.sln >> xlwDotNet9.log  2>&1 
