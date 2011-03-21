d2ed2364ed66289350584c32e7f47f911c91f60a
adcockj <adcockj@948fe115-5b3c-0410-8233-88faca9dced8>
2011-03-21 17:20:42 +0000

    Added x64 builds to vs10 batch file

--- Dev5/xlw/BatchBuild/BuildOnlyVS10.bat
+++ Dev5/xlw/BatchBuild/BuildOnlyVS10.bat
@@ -5,3 +5,6 @@ REM "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcpackages\vcbuild.exe" /r
 
 "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /p:Configuration=Debug /t:rebuild  ..\xlwDotNet\Build\VS10\xlw.Net.sln  >> xlwDotNet10_Debug.log 2>&1 
 "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /p:Configuration=Release /t:rebuild  ..\xlwDotNet\Build\VS10\xlw.Net.sln  >> xlwDotNet10_Release.log 2>&1 
+
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /p:Configuration=Debug;Platform=x64 /t:rebuild  ..\xlw\build\vc10\xlw.sln  >> xlw10_Debug_x64.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /p:Configuration=Release;Platform=x64 /t:rebuild  ..\xlw\build\vc10\xlw.sln  >> xlw10_Release_x64.log 2>&1 
