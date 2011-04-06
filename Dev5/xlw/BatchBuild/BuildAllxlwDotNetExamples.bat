a7d3532805b618425685252cfd8af48ff872562f
adcockj <adcockj@948fe115-5b3c-0410-8233-88faca9dced8>
2011-04-06 11:42:07 +0000

    Build x64 version of example and templates

--- Dev5/xlw/BatchBuild/BuildAllxlwDotNetExamples.bat
+++ Dev5/xlw/BatchBuild/BuildAllxlwDotNetExamples.bat
@@ -21,6 +21,10 @@ set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\
 "C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\Python\VS9\PythonDemo.sln /t:rebuild /property:Configuration=Debug   /property:Platform=x86   >> xlwDotNetPython_Debug_9.log 2>&1 
 "C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\RTDExample\VS9\RTDExample.sln /t:rebuild  /property:Configuration=Debug   /property:Platform=x86  >> xlwDotNetRTDExample_Debug_9.log 2>&1 
 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\Example\VS9\Example.sln /t:rebuild  /property:Configuration=Debug    /property:Platform=x64 >> xlwDotNetExample_Debug_9.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\NonPassive\VS9\NonPassive.sln /t:rebuild /property:Configuration=Debug     /property:Platform=x64 >> xlwDotNetNonPassive_Debug_9.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\Python\VS9\PythonDemo.sln /t:rebuild /property:Configuration=Debug   /property:Platform=x64   >> xlwDotNetPython_Debug_9.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\RTDExample\VS9\RTDExample.sln /t:rebuild  /property:Configuration=Debug   /property:Platform=x64  >> xlwDotNetRTDExample_Debug_9.log 2>&1 
 
 set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\
 
@@ -29,6 +33,7 @@ REM "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\XtraE
 REM "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\XtraExamples\Python\VS10\PythonDemo.sln /t:rebuild /property:Configuration=Debug   /property:Platform=x86   >> xlwDotNetPython_Debug_10.log 2>&1 
 REM "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\XtraExamples\RTDExample\VS10\RTDExample.sln /t:rebuild  /property:Configuration=Debug   /property:Platform=x86  >> xlwDotNetRTDExample_Debug_10.log 2>&1 
 
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\Example\VS10\Example.sln /t:rebuild  /property:Configuration=Debug    /property:Platform=x64 >> xlwDotNetExample_Debug_10.log 2>&1 
 
 
 REM Release
@@ -52,6 +57,10 @@ set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\
 "C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\Python\VS9\PythonDemo.sln /t:rebuild /property:Configuration=Release   /property:Platform=x86   >> xlwDotNetPython_Release_9.log 2>&1 
 "C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\RTDExample\VS9\RTDExample.sln /t:rebuild  /property:Configuration=Release   /property:Platform=x86  >> xlwDotNetRTDExample_Release_9.log 2>&1 
 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\Example\VS9\Example.sln /t:rebuild /property:Configuration=Release   /property:Platform=x64   >> xlwDotNetExample_Release_9.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\NonPassive\VS9\NonPassive.sln /t:rebuild /property:Configuration=Release   /property:Platform=x64   >> xlwDotNetNonPassive_Release_9.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\Python\VS9\PythonDemo.sln /t:rebuild /property:Configuration=Release   /property:Platform=x64   >> xlwDotNetPython_Release_9.log 2>&1 
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild"   ..\xlwDotNet\XtraExamples\RTDExample\VS9\RTDExample.sln /t:rebuild  /property:Configuration=Release   /property:Platform=x64  >> xlwDotNetRTDExample_Release_9.log 2>&1 
 
 set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\
 
@@ -59,6 +68,8 @@ set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\
 REM "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\XtraExamples\NonPassive\VS10\NonPassive.sln /t:rebuild /property:Configuration=Release     /property:Platform=x86 >> xlwDotNetNonPassive_Release_10.log 2>&1 
 REM "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\XtraExamples\Python\VS10\PythonDemo.sln /t:rebuild /property:Configuration=Release   /property:Platform=x86   >> xlwDotNetPython_Release_10.log 2>&1 
 REM "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\XtraExamples\RTDExample\VS10\RTDExample.sln /t:rebuild  /property:Configuration=Release   /property:Platform=x86  >> xlwDotNetRTDExample_Release_10.log 2>&1 
+
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild"   ..\xlwDotNet\Example\VS10\Example.sln /t:rebuild  /property:Configuration=Release    /property:Platform=x64 >> xlwDotNetExample_Release_10.log 2>&1 
 
 
 
