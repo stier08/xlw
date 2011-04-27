964b22d56507f301da0fa20ae4a235eb7c3d4d60
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2011-04-27 05:53:20 +0000

    oops .. forgot to add check these in for logger demo

---
+++ Dev5/xlw/xlw/examples/LoggerDemo/common_source/Test.h
@@ -0,0 +1,22 @@
+//
+//
+//                                                                    Test.h
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <xlw/MyContainers.h>
+#include <xlw/CellMatrix.h>
+#include <xlw/DoubleOrNothing.h>
+#include <xlw/ArgList.h>
+
+
+using namespace xlw;
+
+//<xlw:libraryname=testLogger
+
+double // tests logger
+crand();
+
+#endif // TEST_H
