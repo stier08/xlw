9bf1c26efc6d883dbe05b001d19f9119a70ec760
narinder-claire <narinder-claire@948fe115-5b3c-0410-8233-88faca9dced8>
2011-05-02 20:27:53 +0000

    putting in some spaces inside the brackets .. it's reasonable that it should work

--- Dev5/xlw/xlw/examples/AutoOpenInterception/common_source/Test.cpp
+++ Dev5/xlw/xlw/examples/AutoOpenInterception/common_source/Test.cpp
@@ -19,13 +19,13 @@ void my_open_func()
 	xlw::XlfExcel::MsgBox("Thanks for Opening me!","Welcome");
 }
 
-MacroCache<open>::MacroRegistra my_open_func_r("my_open_func","my_open_func",my_open_func);
+
 
 void my_close_func()
 {
 	xlw::XlfExcel::MsgBox("Bye-Bye ..see you next time!","Bye-Bye");
 }
-MacroCache<close>::MacroRegistra my_close_func_r("my_close_func","my_close_func",my_close_func);
+
 
 extern "C"
 {
