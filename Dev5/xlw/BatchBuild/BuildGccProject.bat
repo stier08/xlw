07cb8866574f94d79962e23c4dfb21650d5a1640
adcockj <adcockj@948fe115-5b3c-0410-8233-88faca9dced8>
2011-05-09 16:20:22 +0000

    Correct log file names for x64 gcc build

--- Dev5/xlw/BatchBuild/BuildGccProject.bat
+++ Dev5/xlw/BatchBuild/BuildGccProject.bat
@@ -4,7 +4,7 @@
 @cd %~p1
 make BUILD=DEBUG PLATFORM=WIN32 rebuild >> "%LOG_ROOT%x86-Debug.log" 2>&1 
 make BUILD=RELEASE PLATFORM=WIN32 rebuild >> "%LOG_ROOT%x86-Release.log" 2>&1 
-make BUILD=DEBUG PLATFORM=x64 rebuild >> "%LOG_ROOT%x86-Debug.log" 2>&1 
-make BUILD=RELEASE PLATFORM=x64 rebuild >> "%LOG_ROOT%x86-Release.log" 2>&1 
+make BUILD=DEBUG PLATFORM=x64 rebuild >> "%LOG_ROOT%x64-Debug.log" 2>&1 
+make BUILD=RELEASE PLATFORM=x64 rebuild >> "%LOG_ROOT%x64-Release.log" 2>&1 
 @popd
 @ENDLOCAL
