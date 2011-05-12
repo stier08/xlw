51d40dfae2a720522db7e480d12bb26299eeb56e
adcockj <adcockj@948fe115-5b3c-0410-8233-88faca9dced8>
2011-05-12 09:40:13 +0000

    Make the build help batch files more robust to spaces in name and provide some diagnostics

--- Dev5/xlw/xlw/build/sandcastle/BuildChmHelp.bat
+++ Dev5/xlw/xlw/build/sandcastle/BuildChmHelp.bat
@@ -1,12 +1,20 @@
-REM ********** Set path for .net framework2.0, sandcastle,hhc,hxcomp****************************
+@if "%~1" == "" goto usage
+@if not exist "%XLW%\xlw\build\%2" goto usage
+@if not exist "Debug\%~1.xll" goto usage
+REM ********** Set path for .net framework2.0, sandcastle,hhc ****************************
+REM assumes default install location for Html Help Workshop
+set TOOLSPATH=%ProgramFiles%
+if exist "%ProgramFiles% (x86)" set TOOLSPATH=%ProgramFiles(x86)%
 
-set PATH=%windir%\Microsoft.NET\Framework\v2.0.50727;%DXROOT%\ProductionTools;c:\program files (x86)\HTML Help Workshop;%PATH%
+if not exist "%TOOLSPATH%\HTML Help Workshop\hhc.exe" goto prereqs
+if not exist "%DXROOT%\ProductionTools" goto prereqs
+if not exist "%windir%\Microsoft.NET\Framework\v2.0.50727" goto prereqs
+
+set PATH=%windir%\Microsoft.NET\Framework\v2.0.50727;%DXROOT%\ProductionTools;%TOOLSPATH%\HTML Help Workshop;%PATH%
 
 cd Debug
 
-call %XLW%\xlw\build\%2\Debug\XlwDocGen.exe %1.xll
-
-pause
+%XLW%\xlw\build\%2\Debug\XlwDocGen.exe "%~1.xll"
 
 if exist output rmdir output /s /q
 
@@ -27,14 +35,46 @@ xcopy output\icons\* chm\icons\ /y /r
 xcopy output\scripts\* chm\scripts\ /y /r
 xcopy output\styles\* chm\styles\ /y /r
 
-ChmBuilder.exe /project:%1 /html:Output\html /lcid:1033 /toc:.\toc.xml /out:Chm /config:.\ChmBuilder.config
+ChmBuilder.exe /project:"%~1" /html:Output\html /lcid:1033 /toc:.\toc.xml /out:Chm /config:.\ChmBuilder.config
 
 DBCSFix.exe /d:Chm /l:1033 
 
 copy %XLW%\xlw\build\sandcastle\alias.h chm\alias.h
 
-hhc chm\%1.hhp
+hhc "chm\%~1.hhp"
 
-copy chm\%1.chm ..
+copy "chm\%~1.chm" ..
 
 cd ..
+
+exit /b 0
+
+:prereqs
+
+@echo **********************************************************************
+@echo Missing Software
+@echo 
+@echo To build help you will need the following software:
+@echo Microsoft Sandcastle
+@echo Microsoft HTML Help Workshop
+@echo .NET 2.0 Runtime
+@echo 
+@echo See the Building Help files section in the Xlw Help file
+@echo 
+@echo If the software is not installed in the default location
+@echo Edit the batch file:
+@echo %XLW%\xlw\build\sandcastle\BuildChmHelp.bat
+@echo **********************************************************************
+
+exit /b 2
+
+:usage
+@echo **********************************************************************
+@echo Usage
+@echo 
+@echo BuildChmHelp.bat ProjectName BuildEnvironment
+@echo ProjectName: should be the name of the xll without extension
+@echo              and the debug version of the xll must have been built
+@echo BuildEnvironment: one of gcc, codeblocks, vc7, vc8, vc9, vc10
+@echo **********************************************************************
+exit /b 1
