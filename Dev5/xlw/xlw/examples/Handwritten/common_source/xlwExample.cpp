d766560963154aaf16dbbb6445766debb520272e
adcockj <adcockj@948fe115-5b3c-0410-8233-88faca9dced8>
2011-05-18 16:57:46 +0000

    Add demo asynchronous function SlowConcat to Handwritten

--- Dev5/xlw/xlw/examples/Handwritten/common_source/xlwExample.cpp
+++ Dev5/xlw/xlw/examples/Handwritten/common_source/xlwExample.cpp
@@ -61,7 +61,6 @@ extern "C" {
     LPXLFOPER EXCEL_EXPORT xlCirc(LPXLFOPER inDiam) {
         EXCEL_BEGIN;
         XlfOper xlDiam(inDiam);
-
         // Converts d to a double.
         double ret = xlDiam.AsDouble();
         // Multiplies it.
@@ -302,6 +301,86 @@ extern "C" {
                                         "Email any questions to xlw-users@lists.sourceforge.net. ");
         EXCEL_END_CMD;
     }
+
+    // demonstrate asynchronous functions
+    // this is function that will be called
+    // it will be called as normal in all versions up to 2007
+    // in Excel 2010 it will be called asynchronously
+    LPXLFOPER EXCEL_EXPORT xlSlowConcat(LPXLFOPER inStr1, LPXLFOPER inStr2) {
+        EXCEL_BEGIN;
+        // pretend something really slow happens
+        Sleep(5000);
+        XlfOper xlStr1(inStr1);
+        XlfOper xlStr2(inStr2);
+
+        // Converts the 2 strings.
+        std::wstring str1 = xlStr1.AsWstring();
+        std::wstring str2 = xlStr2.AsWstring();
+        // Returns the concatenation of the 2 string as an XlfOper.
+        std::wstring ret = str1 + str2;
+        return XlfOper(ret);
+        EXCEL_END;
+    }
+
+    // thread that wraps the normal call
+    // needs to delete the parameter memory once done
+    DWORD WINAPI xlSlowConcatThread(LPVOID args)
+    {
+        XLFOPER* opers = (XLFOPER*)args;
+        try
+        {
+            LPXLFOPER result = xlSlowConcat(&opers[1], &opers[2]);
+            XlfExcel::Instance().Call(xlAsyncReturn, 0, &opers[0], result);
+        }
+        catch(std::exception& e)
+        {
+            XlfOper result(e.what());
+            XlfExcel::Instance().Call(xlAsyncReturn, 0, &opers[0], result);
+        }
+        catch(...)
+        {
+            XlfOper result(XlfOper::Error(xlerrNA));
+            XlfExcel::Instance().Call(xlAsyncReturn, 0, &opers[0], result);
+        }
+
+        xlw::impl::XlfOperProperties<LPXLFOPER>::freeCreatedUsingNew(&opers[1]);
+        xlw::impl::XlfOperProperties<LPXLFOPER>::freeCreatedUsingNew(&opers[2]);
+        TempMemory::FreeMemoryCreatedUsingNew(opers);
+        return 0;
+    }
+
+    // this function is called in Excel 2010 to set-up calculation
+    void EXCEL_EXPORT xlSlowConcatSync(LPXLFOPER inStr1, LPXLFOPER inStr2, LPXLFOPER asyncId) {
+        try
+        {
+            XLFOPER* opers = TempMemory::GetMemoryUsingNew<XLFOPER>(3);
+            xlw::impl::XlfOperProperties<LPXLFOPER>::copyUsingNew(asyncId, &opers[0]);
+            xlw::impl::XlfOperProperties<LPXLFOPER>::copyUsingNew(inStr1, &opers[1]);
+            xlw::impl::XlfOperProperties<LPXLFOPER>::copyUsingNew(inStr2, &opers[2]);
+
+            if (CreateThread(NULL, 0, xlSlowConcatThread, opers, 0, NULL) == NULL)
+            {
+                xlw::impl::XlfOperProperties<LPXLFOPER>::freeCreatedUsingNew(&opers[1]);
+                xlw::impl::XlfOperProperties<LPXLFOPER>::freeCreatedUsingNew(&opers[2]);
+                TempMemory::FreeMemoryCreatedUsingNew(opers);
+                THROW_XLW("Can't create thread");
+            }
+            return;
+        }
+        catch(std::exception& e)
+        {
+            XlfOper result(e.what());
+            XlfOper asyncIdOper(asyncId);
+            XlfExcel::Instance().Call(xlAsyncReturn, 0, asyncIdOper, result);
+        }
+        catch(...)
+        {
+            XlfOper result(XlfOper::Error(xlerrNA));
+            XlfOper asyncIdOper(asyncId);
+            XlfExcel::Instance().Call(xlAsyncReturn, 0, asyncIdOper, result);
+        }
+    }
+
 }
 
 namespace {
@@ -336,7 +415,7 @@ namespace {
 
     XLRegistration::XLFunctionRegistrationHelper registerConcat4(
         "xlConcat4", "Concat4", "Concatenate two strings",
-        "xlw Example", Concat4Args, 2, false, false, "P", "C:\\Source\\xlw\\perso\\john\\Sandcastle\\test\\chm\\test.chm!10002");
+        "xlw Example", Concat4Args, 2);
 
     // Register the function Concat12.
 
@@ -411,4 +490,15 @@ namespace {
     XLRegistration::XLCommandRegistrationHelper registerTestCmd(
         "xlTestCmd", "TestCmd", "A Test Command",
         "Handwritten", "Test Command");
+
+    XLRegistration::Arg SlowConcatArgs[] = {
+        { "string1", "First String", "XLF_OPER" },
+        { "string2", "First String", "XLF_OPER" }
+    };
+
+    XLRegistration::XLFunctionRegistrationHelper registerSlowConcat(
+        "xlSlowConcat", "SlowConcat", "Concatenate a string really slowly to test asynchronous behaviour",
+        "xlw Example", SlowConcatArgs, 2,
+        false, false, "", "", true);
+
 }
